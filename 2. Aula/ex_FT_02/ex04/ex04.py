f57462386c9936839a790a1b89be2344a74b9f64
abaltazarpinto <andrebaltazarpinto@gmail.com>
2024-11-15 11:00:27 +0000

    ex02.py

--- 2. Aula/ex_FT_02/ex04/ex04.py
+++ 2. Aula/ex_FT_02/ex04/ex04.py
@@ -1,4 +1,4 @@
 
 nota1= int(input("Que nota tiveste a português?: "))
 nota2 = int(input("Que not tiveste a geografia?: "))
-print(f"A média das tuas notas {nota1} + {nota2} é {(nota1 + nota2)//2}")
+print(f"A média das tuas notas {nota1} + {nota2} é 10{(nota1 + nota2)//2}")
--- 2. Aula/ex_FT_03/ex01/ex01.py
+++ 2. Aula/ex_FT_03/ex01/ex01.py
@@ -1,3 +1,9 @@
 # I love brave
 
 num = int(input("Enter a whole number: "))
+
+if num > 20:
+    result = num // 2
+    print(f"Number is higher than 20, dividing by 2: {result}")
+else:
+    print(f"Number is {num}, no change needed")
